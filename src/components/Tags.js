ffde04ea9a3408684f8e3634660d895d3dbe253a
Achmad Affandi Fauzan <achmadaffandifauzan@gmail.com>
2025-05-03 17:47:33 +0700

    fix: tag padding

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const GreenTag = ({ text }) => {
   return (
-    <div className="bg-[#87f8dc] px-2.5 pb-0.5 rounded-lg text-xs font-semibold w-fit">
+    <div className="bg-[#87f8dc] px-2.5 py-0.5 rounded-lg text-xs font-semibold w-fit">
       {text}
     </div>
   );
